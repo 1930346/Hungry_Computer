18dbc30db4961ab83578672074abde58d7796142
1930346 <1930346@upv.edu.mx>
2022-04-04 17:14:22 -0500

    Base project, problems model implemented
    Creación base del proyecto, con el primer commit en donde se manda el primer modelo que es problems.

---
+++ hungrycomputer/company/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class Problems(models.Model):
+    #id omitted
+    problem_type = [
+        #Ser+am como los id del problema dude
+        ('1', "Reparación"),
+        ('2', "Mantenimiento"),
+        ('3', "Instalación de software"),
+        ('4', "Otro"),
+    ]
+
+    type = models.CharField(max_length=1, choices=problem_type, default='1')
+    employee_id = models.IntegerField(default = 0)
+    description = models.TextField(max_length=255)
+    date = models.DateTimeField(auto_now_add=True)
