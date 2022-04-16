706fbe1fda3796f20a41373417d915f7beec4c76
AgustinZavalaA <1930120@upv.edu.mx>
2022-04-16 00:06:01 -0500

    vista de almacen funcionando

--- hungrycomputer/company/urls.py
+++ hungrycomputer/company/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     # almacen
     path("almacen", views.almacen_view, name="almacen"),
     path("add_product", views.add_product, name="add_product"),
-    path("update_product/<int:id>", views.update_product, name="update_product"),
+    path("update_product", views.update_product, name="update_product"),
     path("delete_product/<int:id>", views.delete_product, name="delete_product"),
 ]
