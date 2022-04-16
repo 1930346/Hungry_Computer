749a8681a12a5eb1d9792d2860ff96b6f7c2ff8b
1930346 <1930346@upv.edu.mx>
2022-04-15 20:57:34 -0500

    Modelos completados, dos instancias más a fillTable
    Se añaden los modelos completos, se añaden dos instancias más al archivo de fillTable.

--- hungrycomputer/company/fillTable.py
+++ hungrycomputer/company/fillTable.py
@@ -2,7 +2,11 @@ def fill():
     o = Order(id=1, name= 'orden', status= False, attendant= 1, notes= 'nota')
     p = Product(id=1, name= 'memoria', description= 'memoria descricion', model= 'M1500', stock= 50, price= 466.50, serial_num= 'ES23456')
     op = Order_product(id=1, order_id= 1, product_id= 1, authorized_id= 1)
+    a = Addresses(street="Paloma Real", interior_number=None, exterior_number=2927, city="Victoria", state="Tamaulipas", postal_code=87130)
+    c = Client(first_name="Juan", last_name="Perez", phone_number=123456789, email="1930436@upv.edu.mx", address_id=a)
 
+    a.save()
+    c.save()
     o.save()
     p.save()
     op.save()
--- hungrycomputer/company/models.py
+++ hungrycomputer/company/models.py
@@ -19,8 +19,19 @@ class Problem(models.Model):
     date = models.DateTimeField(auto_now_add=True)
 
 
+
+class Client(models.Model):
+    # c = Client(first_name="Juan", last_name="Perez", phone_number=123456789, email="1930436@upv.edu.mx", address_id=a)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    phone_number = models.CharField(max_length=255)
+
+
+#Remember, no russian, na broma, el client_id debe ir en addresses, no viceversa
 class Address(models.Model):
     # a = Addresses(street="Paloma Real", interior_number=None, exterior_number=2927, city="Victoria", state="Tamaulipas", postal_code=87130)
+    client_id = models.ForeignKey(Client)
     street = models.CharField(max_length=255)
     interior_number = models.IntegerField(null=True)
     exterior_number = models.IntegerField()
@@ -29,20 +40,11 @@ class Address(models.Model):
     postal_code = models.IntegerField()
 
 
-class Client(models.Model):
-    # c = Client(first_name="Juan", last_name="Perez", phone_number=123456789, email="1930436@upv.edu.mx", address_id=a)
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    email = models.EmailField(max_length=255)
-    phone_number = models.CharField(max_length=255)
-    address_id = models.ForeignKey(Address, on_delete=models.CASCADE)
-
-
 class Job(models.Model):
-    title = models.TextField(max_length=100)
-    min_salary = models.FloatField(default=0.0)
-    max_salary = models.FloatField(default=0.0)
-
+    title = models.TextField(max_length=100 )
+    min_salary = models.FloatField(default = 0.0)
+    max_salary = models.FloatField(default = 0.0)
+    
 
 class Department(models.Model):
     departments_list = [
@@ -52,42 +54,42 @@ class Department(models.Model):
         ("EB", "Ensamblaje"),
         ("DT", "Distribución"),
         ("AL", "Almacén"),
-        ("FN", "Finanzas"),
+        ("FN", "Finanzas")
     ]
-    name = models.TextField(max_length=100, choices=departments_list, default="RH")
+    name = models.TextField(max_length=100, choices=departments_list, default = "RH")
     description = models.TextField(max_length=255)
 
 
 class Employee(models.Model):
-    sexes = [("M", "Masculino"), ("F", "Femenino"), ("O", "Otro")]
+    sexes=[
+        ("M","Masculino"),
+        ("F","Femenino"),
+        ("O","Otro")
+    ]
 
     first_name = models.TextField(max_length=100)
     last_name = models.TextField(max_length=100)
-    sex = models.CharField(max_length=1, choices=sexes, default="O")
+    sex = models.CharField(max_length=1, choices=sexes,default="O")
     email = models.EmailField(max_length=200)
-    birthdate = models.DateField()
+    date_of_birth = models.DateField()
     hire_date = models.DateField()
-    monthly_salary = models.FloatField(default=0.0)
+    monthly_salary  = models.FloatField(default=0.0)
     bonus = models.FloatField(default=0.0)
-    contract_id = models.IntegerField(max_length=10)
-    department_id = models.ForeignKey(Department, on_delete=models.CASCADE)
-
+    contract_id = models.IntegerField(max_length = 10)
+    department_id = models.ForeignKey(Department)  
+    job_id = models.ForeignKey(Job)
 
 class Order(models.Model):
-    id = models.IntegerField(default=0, primary_key=True)
+    # id = models.IntegerField(default = 0, primary_key=True)
     name = models.TextField(max_length=255)
     status = models.BooleanField(default=False)
     order_date = models.DateTimeField(auto_now_add=True)
-    delivery_date = models.DateTimeField(auto_now_add=True)
-    attendant = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    delivery_date = models.DateTimeField()
+    attendant = models.ForeignKey(Employee)
     notes = models.TextField(max_length=255)
 
-    def __str__(self):
-        self.orders_id
-
-
 class Product(models.Model):
-    id = models.IntegerField(default=0, primary_key=True)
+    # id = models.IntegerField(default=0, primary_key=True)
     name = models.CharField(max_length=255)
     description = models.TextField(max_length=255)
     model = models.TextField(max_length=255)
@@ -95,11 +97,26 @@ class Product(models.Model):
     price = models.FloatField()
     serial_num = models.TextField(max_length=255)
 
+class Order_product(models.Model):
+    # id = models.IntegerField(default = 0, primary_key=True)
+    order_id = models.ForeignKey(Order)
+    product_id = models.ForeignKey(Product)
+    authorized_by = models.ForeignKey(Employee)
+    # authorized_id = models.IntegerField(default = 0, primary_key=True)
+    pickup_date = models.DateTimeField()
 
-class Order_product(object):
-    id = models.IntegerField(default=0, primary_key=True)
-    order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
-    authorized_id = models.IntegerField(default=0, primary_key=True)
-    pickup_date = models.DateTimeField(auto_now_add=True)
-    job_id = models.ForeignKey(Job, on_delete=models.CASCADE)
+
+class Assembly_order(models.Model):
+    attendant = models.ForeignKey(Employee) #ForeignKey with Employee_id
+    finish_date = models.DateTimeField()
+    description = models.TextField(max_length=255)
+    notes = models.TextField(max_length=255)
+    status = models.BooleanField(default=False)
+    client_id = models.ForeignKey(Client)
+
+#Para activos y pasivos
+class Good(models.Model):
+    description = models.TextField(max_length=255)
+    value = models.FloatField(default = 0.0)
+    quantity = models.IntegerField(default = 0)
+    is_active = models.BooleanField(default = True)
