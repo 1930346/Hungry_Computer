08cdc29bf12a319124b23fa25d210e194bbb9a88
AgustinZavalaA <1930120@upv.edu.mx>
2022-04-15 21:24:38 -0500

    cambiando nombres a los que vienen en las vistas (falta agregar el atributo name a HTML)

--- hungrycomputer/company/views.py
+++ hungrycomputer/company/views.py
@@ -36,25 +36,36 @@ def almacen_view(request):
 
 def add_product(request):
     if request.method == "POST":
-        name = request.POST.get("name")
-        price = request.POST.get("price")
-        quantity = request.POST.get("quantity")
+        name = request.POST.get("NombreProd")
+        price = request.POST.get("PrecioProd")
+        quantity = request.POST.get("StockProd")
+        description = request.POST.get("exampleInputNota")
+        serial_num = request.POST.get("IdProd")
+        brand = request.POST.get("MarcaProd")
         print(name, price, quantity)
-        product = Product(name=name, price=price, quantity=quantity)
+        product = Product(
+            name=name, price=price, quantity=quantity, description=description, serial_num=serial_num, brand=brand
+        )
         product.save()
     return redirect(almacen_view)
 
 
 def update_product(request, id):
     if request.method == "POST":
-        name = request.POST.get("name")
-        price = request.POST.get("price")
-        quantity = request.POST.get("quantity")
+        name = request.POST.get("NombreProd")
+        price = request.POST.get("PrecioProd")
+        quantity = request.POST.get("StockProd")
+        description = request.POST.get("exampleInputNota")
+        serial_num = request.POST.get("IdProd")
+        brand = request.POST.get("MarcaProd")
         print(name, price, quantity)
         product = Product.objects.get(id=id)
         product.name = name
         product.price = price
         product.quantity = quantity
+        product.description = description
+        product.serial_num = serial_num
+        product.brand = brand
         product.save()
     return redirect(almacen_view)
 
